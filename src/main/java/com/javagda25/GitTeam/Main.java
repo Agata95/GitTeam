c7682f6d53a8c6c4c83965e3f1f04ab7b727125d
Agata <agata.weltrowska@outlook.com>
2019-06-11 20:17:24 +0200

    commit

--- src/main/java/com/javagda25/GitTeam/Main.java
+++ src/main/java/com/javagda25/GitTeam/Main.java
@@ -1,8 +1,16 @@
 package com.javagda25.GitTeam;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
     }
 
+    public int skladanieZamowien(){
+        Produkt produkt = new Produkt();
+        Scanner scanner = new Scanner(System.in);
+
+    }
 }
--- src/main/java/com/javagda25/GitTeam/Produkt.java
+++ src/main/java/com/javagda25/GitTeam/Produkt.java
@@ -2,9 +2,11 @@ package com.javagda25.GitTeam;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 
 public class Produkt {
     private String nazwa;
