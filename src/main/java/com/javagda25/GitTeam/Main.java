a0755a053c4b2f5f059df491b978e14916b5ebef
Kasia Kaczmarek <katarzyna.dobrzeniecka@gmail.com>
2019-06-11 20:21:22 +0200

    dodanie klasy magazyn

--- src/main/java/com/javagda25/GitTeam/Main.java
+++ src/main/java/com/javagda25/GitTeam/Main.java
@@ -1,8 +1,31 @@
 package com.javagda25.GitTeam;
 
+//Podaj komendę:
+//        -> dodaj dostawę
+//        Podaj numer zamówienia:
+//        -> GD234/4
+//        Zamówienie zawiera 3 produkty.
+//        Czy w dostawie znajduje się produkt: chleb, cena 2.3, ilość 5?
+//        -> tak
+//        Czy w dostawie znajduje się produkt: masło, cena 2.0, ilość 2?
+//        -> tak
+//        Czy w dostawie znajduje się produkt: ser, cena 0.5, ilość 23.0?
+//        -> nie
+//        Oznaczam produkt jako niedostarczony, nie zwiększam stanu magazynowego.
+//        Zamówienie zrealizowane, podaj numer faktury:
+//        -> FV235 / 30
+//        Faktura dopisana do zamówienia.
+//        Czy chcesz wprowadzić datę dostawy ręcznie?
+//        -> nie
+//        Data dostawy zaktualizowana na obecną automatycznie.
+//        [na podstawie daty powinna być wyliczana długość spóźnienia, domyślnie każde zamówienie musi być dostarczone w ciągu 1 min.
+//        jeśli nie jest, to informacja o opóźnieniu powinna być dopisana]
+
 public class Main {
     public static void main(String[] args) {
 
-    }
 
+
+
+    }
 }
